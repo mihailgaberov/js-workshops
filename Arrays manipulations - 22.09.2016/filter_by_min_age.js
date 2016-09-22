8011e02d60eaf1629e26552d1255165662e764e6
Mihail Gaberov <mihail.gaberov@gmail.com>
2016-09-23 00:23:59 +0200

    Workshop 22.09.2016

--- Arrays manipulations - 22.09.2016/filter_by_min_age.js
+++ Arrays manipulations - 22.09.2016/filter_by_min_age.js
@@ -7,37 +7,32 @@ The output should be printed to the console.
 
 */
 
-'use strict';
-class FilterByMinAge {
-    constructor(arrInput) {
-    	if (arrInput === undefined || arrInput.length === 0) {
-    		throw new Error('Invalid input data.');
-    	}
+function filterByMinAge(arrInput) {
+	"use strict";
+
+	if (arrInput === undefined || arrInput.length === 0) {
+		throw new Error('Invalid input data.');
+	}
 
 
-        let minAge = Number(arrInput[0]);
-		
-		let objPerson1 = {
-			name: arrPersons[1],
-			age: Number(arrPersons[2])
-		};
+    let minAge = Number(arrInput[0]);
+	
+	let objPerson1 = {
+		name: arrInput[1],
+		age: Number(arrInput[2])
+	};
 
-		let objPerson2 = {
-			name: arrPersons[3],
-			age: Number(arrPersons[4])
-		};
+	let objPerson2 = {
+		name: arrInput[3],
+		age: Number(arrInput[4])
+	};
 
-		if (objPerson1.age >= minAge && objPerson2.age < minAge) {
-			return objPerson1;
-		} else {
-			return objPerson2;
-		}	
-    }
+	if (objPerson1.age >= minAge && objPerson2.age < minAge) {
+		console.log(objPerson1);
+		return objPerson1;
+	} else {
+		return objPerson2;
+	}
 }
 
-
-if (require.main === module) {
-    let output = new FilterByMinAge(['12', 'Ivan', '15', 'Asen', '9']);
-
-    console.log(output);
-}
+filterByMinAge(['12', 'Ivan', '15', 'Asen', '9']);
