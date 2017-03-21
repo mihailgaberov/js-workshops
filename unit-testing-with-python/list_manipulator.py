a4328161d73b8f00790997048b45451b5ea5f67d
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-03-21 23:39:04 +0100

    Adding unit test with Python - same_first_last()

--- unit-testing-with-python/list_manipulator.py
+++ unit-testing-with-python/list_manipulator.py
@@ -1,2 +1,5 @@
 def first_last6(nums):
   return nums[0] == 6 or nums[len(nums)-1] == 6
+
+def same_first_last(nums):
+  return len(nums) >= 1 and nums[0] == nums[len(nums) - 1]
--- unit-testing-with-python/list_manipulator.test.py
+++ unit-testing-with-python/list_manipulator.test.py
@@ -6,5 +6,11 @@ class TestListMethods(unittest.TestCase):
     def test_first_last6(self):
       self.assertEqual(list_manipulator.first_last6([1, 3, 6]), True)
 
+    def test_same_first_last(self):
+      self.assertEqual(list_manipulator.same_first_last([1, 2, 3]), False)
+      self.assertEqual(list_manipulator.same_first_last([1, 2, 3, 1]), True)
+      self.assertEqual(list_manipulator.same_first_last([1, 2, 1]), True)
+      self.assertEqual(list_manipulator.same_first_last([]), False)
+
 if __name__ == '__main__':
     unittest.main()
